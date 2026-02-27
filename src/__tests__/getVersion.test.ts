cfcf0f676b83c2ebbc877f86a1079abe4b2e7647
Tomoki-13 <s266016@wakayama-u.ac.jp>
2026-02-27 14:19:57 +0900

    検出に使う関数追加

--- src/__tests__/getVersion.test.ts
+++ src/__tests__/getVersion.test.ts
@@ -1,4 +1,4 @@
-import {_privateForTest ,getVersion} from '../module/getVersion';
+import { _privateForTest, getVersion } from '../git/getVersion';
 import path from 'path';
 
 //テストファイル一覧：絶対パス
@@ -6,22 +6,22 @@ const dirPath1 = path.join(process.cwd(), './src/__tests__/inputFiles');
 const dirPath2 = path.join(process.cwd(), './src/__tests__/inputFiles', 'samplerepo');
 const filePath1 = path.join(process.cwd(), './src/__tests__/inputFiles', 'samplerepo', 'package.json');
 describe('checkDepend', () => {
-    test('default', () => {
-        let expectedOutput = "^0.0.1-security";
-        expect(_privateForTest.checkDepend(filePath1,'fs')).toEqual(expectedOutput);
-    });
-    test('input unused library ', () => {
-        let expectedOutput = "no";
-        expect(_privateForTest.checkDepend(filePath1,'lib')).toEqual(expectedOutput);
-    });
+  test('default', () => {
+    let expectedOutput = "^0.0.1-security";
+    expect(_privateForTest.checkDepend(filePath1, 'fs')).toEqual(expectedOutput);
+  });
+  test('input unused library ', () => {
+    let expectedOutput = "no";
+    expect(_privateForTest.checkDepend(filePath1, 'lib')).toEqual(expectedOutput);
+  });
 });
 
 describe('findPackageJson', () => {
-    test('default', () => {
-        expect(_privateForTest.findPackageJson(dirPath2)).toEqual(filePath1);
-    });
-    test('input mainrepo package.json ', () => {
-        let expectedOutput = path.resolve(process.cwd(), 'package.json');
-        expect(_privateForTest.findPackageJson(dirPath1)).toEqual(expectedOutput);
-    });
+  test('default', () => {
+    expect(_privateForTest.findPackageJson(dirPath2)).toEqual(filePath1);
+  });
+  test('input mainrepo package.json ', () => {
+    let expectedOutput = path.resolve(process.cwd(), 'package.json');
+    expect(_privateForTest.findPackageJson(dirPath1)).toEqual(expectedOutput);
+  });
 });
--- src/core/extractVersion.ts
+++ src/core/extractVersion.ts
@@ -1,53 +1,53 @@
 import fs from "fs";
 import path from 'path';
 import output_json from "../utils/output_json";
-import { cloneRepo } from "../module/cloneRepo";
-import { checkoutCommit } from "../module/checkoutCommit";
+import { cloneRepo } from "../git/cloneRepo";
+import { checkoutCommit } from "../git/checkoutCommit";
 import { Client_Ver } from "../types/VersionCommits";
 
 // リポジトリを順番にクローン
 //libVersionはファイル名を変更するためだけに使用(client_listでフィルタリング済み)
-export const extractVersion = async (client_list:string[],libName:string,libNum:string = '0',state:string = ''):
-    Promise<Client_Ver[]> => {
-    //process.cwd() == src
-    let std_Dir:string = path.resolve(process.cwd(), '../clientRepos/'); 
-    // 出力先ディレクトリの作成
-    output_json.createOutputDirectory(std_Dir);
-    // ライブラリ名のディレクトリを作成 clientRepos/libName
-    let cloneDir = path.join(std_Dir, libName);
-    output_json.createOutputDirectory(cloneDir);
-    console.log(process.cwd());
-    let verHistory:Client_Ver[] = [];
+export const extractVersion = async (client_list: string[], libName: string, libNum: string = '0', state: string = ''):
+  Promise<Client_Ver[]> => {
+  //process.cwd() == src
+  let std_Dir: string = path.resolve(process.cwd(), '../clientRepos/');
+  // 出力先ディレクトリの作成
+  output_json.createOutputDirectory(std_Dir);
+  // ライブラリ名のディレクトリを作成 clientRepos/libName
+  let cloneDir = path.join(std_Dir, libName);
+  output_json.createOutputDirectory(cloneDir);
+  console.log(process.cwd());
+  let verHistory: Client_Ver[] = [];
 
-    //クライアントのリストを600に制限
-    let limit = 1;
-    let count = 0;
+  //クライアントのリストを600に制限
+  // let limit = 5;
+  let count = 0;
 
-    //ぞれぞれにクローン，チェックアウト，バージョン確認を実行
-    for(const client of client_list) {
-        // if(count >= limit) {
-        //     console.log(`クライアントの取得を終了します。`);
-        //     break;
-        // }else{
-        //     console.log(`クライアントの取得を続行します。残り${limit - count}件`);
-        // }
-        try{
-            let repoPath = await cloneRepo(client,cloneDir);
-            if (!repoPath) {
-                console.warn(`clone failure: ${client}`);
-                continue;
-            }
-            let c_data:Client_Ver = await checkoutCommit(repoPath, libName);
-            //バージョン数が最低でも2以上のものを取得　変更しているものに限定
-            if(c_data && c_data.verList.length > 1) {
-                verHistory =  verHistory.concat(c_data);
-            }
-            process.chdir(std_Dir);
-            count++;
-        } catch (error) {
-            console.error(error);
-            count++;
-        }
+  //ぞれぞれにクローン，チェックアウト，バージョン確認を実行
+  for (const client of client_list) {
+    // if(count >= limit) {
+    //     console.log(`DEBUG:クライアントの取得を終了します。`);
+    //     break;
+    // }else{
+    //     console.log(`クライアントの取得を続行します。残り${limit - count}件`);
+    // }
+    try {
+      let repoPath = await cloneRepo(client, cloneDir);
+      if (!repoPath) {
+        console.warn(`clone failure: ${client}`);
+        continue;
+      }
+      let c_data: Client_Ver = await checkoutCommit(repoPath, libName);
+      //バージョン数が最低でも2以上のものを取得　変更しているものに限定
+      if (c_data && c_data.verList.length > 1) {
+        verHistory = verHistory.concat(c_data);
+      }
+      process.chdir(std_Dir);
+      count++;
+    } catch (error) {
+      console.error(error);
+      count++;
     }
-    return verHistory;
+  }
+  return verHistory;
 };
